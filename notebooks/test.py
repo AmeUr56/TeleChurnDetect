279b7818577309a0a545168610926640f3445127
Ameur56 <boughershaouari@gmail.com>
2024-12-24 08:45:51 +0100

    Enhanced Pipeline

--- notebooks/test.py
+++
@@ -1,8 +0,0 @@
-import dill
-import pandas as pd
-
-data = pd.read_csv("../data/train_processed_data.csv")
-with open("pipeline.dll", 'rb') as f:
-    pipeline =  dill.load(f)
-
-pipeline.run(data)
--- pipelines/pipeline.py
+++ pipelines/pipeline.py
@@ -1,41 +1,69 @@
-from prefect import task,flow
+#from prefect import task,flow
 import pandas as pd
 from joblib import load
+import numpy as np
 
-@task
+import logging
+
+# Setup Logging
+# Creating Logger
+pipeline_logger = logging.getLogger("pipeline_logger")
+pipeline_logger.setLevel(logging.DEBUG)
+
+# Adding Handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+# Setting Formats
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+# Apply the formatter to the console handler
+console_handler.setFormatter(formatter)
+# Attach the handler to the logger
+pipeline_logger.addHandler(console_handler)
+
+#@task
 def load_data(data):
+    pipeline_logger.DEBUG("Loding Data")
     data = pd.DataFrame(data,columns=['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure',
        'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity',
        'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',
        'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod',
        'MonthlyCharges', 'TotalCharges', 'Churn'])
+    
+    pipeline_logger.INFO("Data Loaded Succefuly")
+
     return data
 
 mode_features = ["gender","SeniorCitizen","Partner","Dependents","PhoneService","MultipleLines","InternetService","OnlineSecurity","OnlineBackup","DeviceProtection","TechSupport","StreamingTV","StreamingMovies","Contract","PaperlessBilling","PaymentMethod"]
 mean_features = ["tenure","MonthlyCharges","TotalCharges"]
 mode_imputer = load("../pipelines/mode_imputer.joblib")
 mean_imputer = load("../pipelines/mean_imputer.joblib")
-@task
+#@task
 def clean_data(org_data):
     data = org_data.copy()
     mode_features = ["gender","SeniorCitizen","Partner","Dependents","PhoneService","MultipleLines","InternetService","OnlineSecurity","OnlineBackup","DeviceProtection","TechSupport","StreamingTV","StreamingMovies","Contract","PaperlessBilling","PaymentMethod"]
     mean_features = ["tenure","MonthlyCharges","TotalCharges"]
-
+    
+    pipeline_logger.DEBUG("Cleaning Data")
+    
     mode_cleaned_data = pd.DataFrame(mode_imputer.transform(data[mode_features]),columns=mode_features)
     mean_cleaned_data = pd.DataFrame(mean_imputer.transform(data[mean_features]),columns=mean_features)
+    cleaned_data = pd.concat([mode_cleaned_data,mean_cleaned_data],axis=1)
 
-    return pd.concat([mode_cleaned_data,mean_cleaned_data],axis=1)
-
-@task
-def feature_engineering(org_data):
-    if isinstance(org_data,pd.DataFrame):
-        data = org_data.copy()
+    if sum(cleaned_data.isnull().sum()):
+        pipeline_logger.DEBUG("Data Cleaning Failed")
     else:
-        data = pd.DataFrame(org_data,columns=['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure',
-       'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity',
-       'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',
-       'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod',
-       'MonthlyCharges', 'TotalCharges', 'Churn'])
+        pipeline_logger.INFO("Data Cleaned Succefuly")
+    return 
+
+#@task
+def feature_engineering(org_data):
+    data = pd.DataFrame(org_data,columns=['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure',
+    'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity',
+    'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',
+    'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod',
+    'MonthlyCharges', 'TotalCharges', 'Churn'])
         
     data['TenureCategory'] = pd.cut(data['tenure'], bins=[-1,3,12,np.inf],labels=["Recent", "Established", "Loyal"]) 
     data['Tenure_X_MonthlyCharges'] = data['tenure'] * data['MonthlyCharges']
@@ -50,35 +78,41 @@ def feature_engineering(org_data):
                                     (data['InternetService'] == "Yes")
     data['Connected'] = (data['Partner'] == "Yes") & (data['Dependents'] == "Yes")
     
+    pipeline_logger.INFO("Feature Engineering Succeced")
+
     return data
 
 ordinal_features = ["SeniorCitizen","Partner","Dependents","PhoneService","MultipleLines","InternetService","OnlineSecurity","OnlineBackup","DeviceProtection","TechSupport","StreamingTV","StreamingMovies","Contract","PaperlessBilling","TenureCategory","HighSpender","AutomaticPayment","AllServicesActivated","TeleServicesActivated","Connected"]
 nominal_features = ["gender","PaymentMethod"]
 ord_enc = load("../pipelines/ord_enc.joblib")
 one_hot_enc = load("../pipelines/one_hot_enc.joblib")
-@task
+#@task
 def encoding_data(org_data):
     data = org_data.copy()
     ordinal_features = ["SeniorCitizen","Partner","Dependents","PhoneService","MultipleLines","InternetService","OnlineSecurity","OnlineBackup","DeviceProtection","TechSupport","StreamingTV","StreamingMovies","Contract","PaperlessBilling","TenureCategory","HighSpender","AutomaticPayment","AllServicesActivated","TeleServicesActivated","Connected"]
     nominal_features = ["gender","PaymentMethod"]
 
     data[ordinal_features] = ord_enc.transform(data[ordinal_features])
-    
+    pipeline_logger.INFO("Data Ordinal Encoded Succefuly")
+
     one_hot_encoded_data = one_hot_enc.transform(data[nominal_features])
     one_hot_encoded_data_columns = one_hot_enc.get_feature_names_out(input_features=data[nominal_features].columns)
     one_hot_data = pd.DataFrame(one_hot_encoded_data,columns=one_hot_encoded_data_columns)
-    
+    pipeline_logger.INFO("Data One Hot Encoded Succefuly")
+
     return pd.concat([data,one_hot_data],axis=1).drop(nominal_features,axis=1)
 
 num_features = ["tenure","MonthlyCharges","TotalCharges","MonthlyChargesRatio","Tenure_X_MonthlyCharges","SqrtTotalCharges","SqrtMonthlyCharges"]
 scaler = load("../pipelines/scaler.joblib")
-@task
+#@task
 def scaling_data(data):
     data[num_features] = scaler.transform(data[num_features])
 
+    pipeline_logger.INFO("Data Scaled Succefuly")
+
     return data
 
-@flow(name="Pipeline")
+#@flow(name="Pipeline")
 def pipeline(data):
     processed_data = load_data(data)
     processed_data = clean_data(processed_data)
